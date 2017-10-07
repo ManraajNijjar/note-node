c3527c50d6ee27220b96fdfe97e16ead15d7443c
Manraaj Nijjar <ManraajNijjar@gmail.com>
2017-10-06 21:42:42 -0400

    Feat: Went through relearning the process for requiring files and exporting functions

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-console.log('Starting App');
+console.log('Starting App.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
+const notes = require('./notes.js');
 
 var user = os.userInfo();
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!`, function (err){
-  if (err) {
-    console.log('Unable to write file');
-  }
-});
+
+var filteredArray = _.uniq(['Hello', 'Mello', 'Hello', 'hello', 1, 2, 3, 4, 4]);
+console.log(filteredArray);
+//console.log(notes.add(7, 9));
+
+// var res = notes.addNote;
+// console.log(res);
+
+// fs.appendFile('greetings.txt', `Hello ${user.username}!`, function (err){
+//   if (err) {
+//     console.log('Unable to write file');
+//   }
+// });
---
+++ notes.js
@@ -0,0 +1,10 @@
+console.log('Starting Notes.js');
+
+module.exports.addNote = () => {
+  console.log('addNotes');
+  return 'New note';
+}
+
+module.exports.add = (a, b) => {
+  return a + b
+}
